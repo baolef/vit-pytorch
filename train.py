d03527ddb51f488f75e093584506fd5f413cf78e
Baole Fang <baole.fang@gmail.com>
2024-04-14 18:48:14 -0400

    feat: better wandb run name

--- train.py
+++ train.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str, default='configs/cifar100.yaml', help='path to the configuration file')
     parser.add_argument('--gpus', type=str, default='0', help='gpus to use')
-    parser.add_argument('--overwrite', action='store_true', help='overwrite existing experiment')
+    parser.add_argument('--force', action='store_true', help='overwrite existing experiment')
     args = parser.parse_args()
 
     with open(args.config, 'r') as f:
@@ -104,7 +104,7 @@ if __name__ == '__main__':
 
     output_root = os.path.join(config['output_dir'], config['experiment'])
     if os.path.exists(output_root) and not config['resume']:
-        if not args.overwrite:
+        if not args.force:
             print(f'Experiment {config["experiment"]} already exists. Enter y to overwrite.')
             choice = input()
             if choice != 'y':
